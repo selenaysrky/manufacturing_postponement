6d86e12ecbf4f92c87cc74975c215b9d62510084
selenaysrky <selenaysrky@gmail.com>
2024-01-16 19:54:40 +0300

    days are divdided by months

--- coding/manufacturing_postponement.R
+++ coding/manufacturing_postponement.R
@@ -1,12 +1,12 @@
-#Reading the imported Excel file
+#Read the imported Excel file
 library(readxl)
 november_orders <- read_excel("C:/Users/tt055367/Desktop/Bitirme/Data/november_orders.xlsx")
 View(november_orders)
 
-#Renaming the file
+#Rename the file
 nov_po <- november_orders
 
-#Checking if there exists NA's
+#Check if there exists NA's
 sum(is.na(nov_po$`Müşteri siparişi`))
 sum(is.na(nov_po$'Ötelenmemesi Gereken Sipariş'))
 sum(is.na(nov_po$'Gövde Stok'))
@@ -19,31 +19,20 @@ sum(is.na(nov_po$'Pln.bşl.termini'))
 sum(is.na(nov_po$'Yeni Tarih'))
 sum(is.na(nov_po$'Seri'))
 
-#Removing the NA's
+#Remove the NA's
 clean_siparis <- na.omit(nov_po$`Müşteri siparişi`)
-sum(is.na(clean_siparis))
 clean_otelemeYok <- na.omit(nov_po$`Ötelenmemesi Gereken Sipariş`)
-sum(is.na(clean_otelemeYok))
 clean_govde <- na.omit(nov_po$'Gövde Stok')
-sum(is.na(clean_govde))
 clean_kabin <- na.omit(nov_po$'Kabin Stok')
-sum(is.na(clean_kabin))
 clean_TR6 <- na.omit(nov_po$'TR6 Gövde')
-sum(is.na(clean_TR6))
 clean_versiyon <- na.omit(nov_po$'Üretim versiyonu')
-sum(is.na(clean_versiyon))
 clean_organizasyon <- na.omit(nov_po$'Satış organizasyonu')
-sum(is.na(clean_organizasyon))
 clean_bitis <- na.omit(nov_po$'Fiili btş.trm.')
-sum(is.na(clean_bitis))
 clean_baslangic <- na.omit(nov_po$'Pln.bşl.termini')
-sum(is.na(clean_baslangic))
 clean_yeniTarih <- na.omit(nov_po$'Yeni Tarih')
-sum(is.na(clean_yeniTarih))
 clean_seri <- na.omit(nov_po$'Seri')
-sum(is.na(clean_seri))
 
-#Checking if the removal is successful
+#Check if the removal is successful
 sum(is.na("TR6 Gövde"))
 sum(is.na("Gövde Stok"))
 
@@ -52,10 +41,23 @@ sum(is.na("Gövde Stok"))
 nov_po_filtered <- filter(nov_po, `Gövde Stok` == "0" & `Kabin Stok` == "0")
 
 
-# Generate all days of the year 2024
-days_2024 <- seq(as.Date("2024-01-01"), as.Date("2024-12-31"), by = "days")
+# Load required libraries
+library(lubridate)
+library(dplyr)
 
-# Create a data frame
-days_df <- data.frame(Date = days_2024)
+# Create a data frame with all the days of 2024
+days_2024 <- data.frame(Date = seq(as.Date("2024-01-01"), as.Date("2024-12-31"), by = "days"))
 
+# Extract month and year information
+days_2024 <- days_2024 %>%
+  mutate(Month = month(Date), Year = year(Date))
+
+# Change date format to "31/12/2024"
+days_2024$Date <- format(days_2024$Date, "%d/%m/%Y")
+
+# Split the data frame by month
+days_by_month <- split(days_2024, f = days_2024$Month)
+
+# Display the first few rows of each month's data frame
+lapply(days_by_month, function(month_df) head(month_df))
 
